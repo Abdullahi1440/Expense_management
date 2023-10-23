a6c6625db07485c6ba5f1d3092067b26a11f1ea8
=Abdullahi <guure1440@gmail.com>
2023-10-23 10:04:05 +0300

    Report UI

---
+++ Application/view/get_user_statement.php
@@ -0,0 +1,91 @@
+                <?php
+
+                include('header.php');
+                include('sidebar.php');
+            
+
+                ?>
+                <div class="pcoded-main-container">
+                    <div class="pcoded-wrapper">
+                        <div class="pcoded-content">
+                            <div class="pcoded-inner-content">
+                                <!-- [ breadcrumb ] start -->
+
+                                <!-- [ breadcrumb ] end -->
+                                <div class="main-body">
+                                    <div class="page-wrapper">
+                                        <!-- [ Main Content ] start -->
+                                        <!-- [ start row ]  -->
+                                        <div class="row">
+                                            <div class="col-xl-12">
+                                                <div class="card">
+                                                    <div class="card-header">
+                                                        <h5>Basic Table</h5>
+
+                                                    </div>
+                                                    <div class="row">
+                                                        <div class="col-sm-4">
+                                                            <select text="type" name="type"  id="type"class="form-control">
+                                                                <option value="0">All</option>
+                                                                <option value="custome">custome</option>
+                                                            </select>
+                                                        </div>
+                                                        <div class="col-sm-4">
+                                                            <input type="date" name="from" id="from" class="form-control">
+                                                            
+                                                        </div>
+                                                        <div class="col-sm-4">
+                                                            <input type="date" name="to" id="to" class="form-control">
+
+                                                        </div>
+
+                                                    </div>
+                                                    <div class="card-block table-border-style">
+                                                        <div class="table-responsive">
+                                                            <button class="btn btn-success float-right" id="AddNew"> Add New transtion </button>
+                                                            <table class="table" id="">
+                                                                <thead>
+                                                                    <tr>
+                                                                        <th>#</th>
+                                                                        <th>Amount</th>
+                                                                        <th>Type</th>
+                                                                        <th>Description</th>
+                                                                        <th>User</th>
+                                                                        <th>Date</th>
+                                                                        <th>Action</th>
+
+                                                                    </tr>
+                                                                </thead>
+                                                                <tbody>
+                                                                    
+
+                                                                </tbody>
+
+                                                            </table>
+
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <!-- [row] end -->
+                                       
+
+
+                                        <!-- [ Main Content ] end -->
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                <?php
+
+                include('footer.php');
+
+
+                ?>
+                <script src="../js/user_statement.js"></script>
+                <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
